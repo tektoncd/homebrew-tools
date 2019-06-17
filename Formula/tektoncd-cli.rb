b5347c107a449467bf5f3cb798be9a588911b9b2
Chmouel Boudjnah <chmouel@chmouel.com>
2019-06-17 16:05:51 +0200

    Remove url doublons
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- Formula/tektoncd-cli.rb
+++ Formula/tektoncd-cli.rb
@@ -2,7 +2,6 @@
 class TektoncdCli < Formula
   desc ""
   homepage "https://tekton.dev"
-  url "https://github.com/tektoncd/cli"
   version "0.1"
   sha256 "c43e6d776a5b532ba7587b4695b31366cea2769a7dd7ce971aba6b11f34b18b7"
   url "https://github.com/tektoncd/cli/releases/download/v0.1.0/cli_0.1.0_Darwin_x86_64.tar.gz"
