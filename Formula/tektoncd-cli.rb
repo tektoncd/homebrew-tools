3a692b6ea87eb773f246904bbf54025474675443
Chmouel Boudjnah <chmouel@chmouel.com>
2019-06-17 16:01:18 +0200

    No need to compile, should have listent to sunil and vincent :-p

--- Formula/tektoncd-cli.rb
+++ Formula/tektoncd-cli.rb
@@ -6,12 +6,8 @@ class TektoncdCli < Formula
   version "0.1"
   sha256 "c43e6d776a5b532ba7587b4695b31366cea2769a7dd7ce971aba6b11f34b18b7"
   url "https://github.com/tektoncd/cli/releases/download/v0.1.0/cli_0.1.0_Darwin_x86_64.tar.gz"
-  depends_on "go" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "go build ./cmd/..."
     bin.install "tkn" => "tkn"
   end
 
